8c31baf57c06b210aefe06efcccccfe7558a2257
Adrian K <me@adrianpk.com>
2023-06-28 16:49:02 +0200

    Migrator updated

--- backend/internal/infra/migration/sqlite/queries.go
+++ backend/internal/infra/migration/sqlite/queries.go
@@ -12,23 +12,17 @@ const (
 	createMigraationTableSt = `CREATE TABLE %s (
     id UUID PRIMARY KEY,
 		name VARCHAR(64),
-		up_fx VARCHAR(64),
-		down_fx VARCHAR(64),
- 		is_applied BOOLEAN,
-		created_at TIMESTAMP
+		created_at TEXT
 	);`
 
 	sqlDropMigrationsSt = `DROP TABLE %s;`
 
-	selMigrationSt = `
-	SELECT is_applied FROM %s 
-	                  WHERE name = '%s' and is_applied = true`
+	selMigrationSt = `SELECT (COUNT(*) > 0) AS record_exists FROM %s WHERE name = '%s'`
 
 	recMigrationSt = `
-	INSERT INTO %s (id, name, up_fx, down_fx, is_applied, created_at)
-	VALUES (:id, :name, :up_fx, :down_fx, :is_applied, :created_at);`
+	INSERT INTO %s (id, name, created_at)
+	VALUES (:id, :name, :created_at);`
 
 	delMigrationSt = `DELETE FROM %s 
-       WHERE name = '%s' 
-           AND is_applied = true`
+       WHERE name = '%s'`
 )
