4743c8726f72c723c92e7c79b2c68929c96c8277
Adrian K <me@adrianpk.com>
2023-06-30 14:43:32 +0200

    Testing foundation

--- backend/internal/sys/log/mock.go
+++ backend/internal/sys/log/mock.go
@@ -6,19 +6,19 @@ import (
 )
 
 type TestLogger struct {
-	debugLogs *bytes.Buffer
-	infoLogs  *bytes.Buffer
-	errorLogs *bytes.Buffer
-	logLevel  LogLevel
+	debug    *bytes.Buffer
+	info     *bytes.Buffer
+	error    *bytes.Buffer
+	logLevel LogLevel
 }
 
 func NewTestLogger(logLevel string) *TestLogger {
 	level := ToValidLevel(logLevel)
 	return &TestLogger{
-		debugLogs: bytes.NewBuffer(nil),
-		infoLogs:  bytes.NewBuffer(nil),
-		errorLogs: bytes.NewBuffer(nil),
-		logLevel:  level,
+		debug:    bytes.NewBuffer(nil),
+		info:     bytes.NewBuffer(nil),
+		error:    bytes.NewBuffer(nil),
+		logLevel: level,
 	}
 }
 
@@ -29,56 +29,56 @@ func (l *TestLogger) SetLogLevel(level LogLevel) {
 func (l *TestLogger) Debug(v ...interface{}) {
 	if l.logLevel <= Debug {
 		message := fmt.Sprintln(v...)
-		l.debugLogs.WriteString(message)
+		l.debug.WriteString(message)
 	}
 }
 
 func (l *TestLogger) Debugf(format string, a ...interface{}) {
 	if l.logLevel <= Debug {
 		message := fmt.Sprintf(format, a...)
-		l.debugLogs.WriteString(message)
+		l.debug.WriteString(message)
 	}
 }
 
 func (l *TestLogger) Info(v ...interface{}) {
 	if l.logLevel <= Info {
 		message := fmt.Sprintln(v...)
-		l.infoLogs.WriteString(message)
+		l.info.WriteString(message)
 	}
 }
 
 func (l *TestLogger) Infof(format string, a ...interface{}) {
 	if l.logLevel <= Info {
 		message := fmt.Sprintf(format, a...)
-		l.infoLogs.WriteString(message)
+		l.info.WriteString(message)
 	}
 }
 
 func (l *TestLogger) Error(v ...interface{}) {
 	if l.logLevel <= Error {
 		message := fmt.Sprintln(v...)
-		l.errorLogs.WriteString(message)
+		l.error.WriteString(message)
 	}
 }
 
 func (l *TestLogger) Errorf(format string, a ...interface{}) {
 	if l.logLevel <= Error {
 		message := fmt.Sprintf(format, a...)
-		l.errorLogs.WriteString(message)
+		l.error.WriteString(message)
 	}
 }
 
 // GetDebugLogs returns the captured debug logs.
 func (l *TestLogger) GetDebugLogs() string {
-	return l.debugLogs.String()
+	return l.debug.String()
 }
 
 // GetInfoLogs returns the captured info logs.
 func (l *TestLogger) GetInfoLogs() string {
-	return l.infoLogs.String()
+	return l.info.String()
 }
 
 // GetErrorLogs returns the captured error logs.
 func (l *TestLogger) GetErrorLogs() string {
-	return l.errorLogs.String()
+	return l.error.String()
 }
