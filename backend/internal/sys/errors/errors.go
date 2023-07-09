2ab33da21cb83d4302ee2a206c2813fcabb81601
Adrian K <me@adrianpk.com>
2023-07-09 22:52:15 +0200

    Improved error handling

--- backend/internal/sys/errors/errors.go
+++ backend/internal/sys/errors/errors.go
@@ -1,16 +1,73 @@
 package errors
 
-import "fmt"
-
-type (
-	errors struct{}
+import (
+	"errors"
+	"fmt"
+	"runtime"
+	"strings"
 )
 
-func Wrap(err error, message string) error {
-	return fmt.Errorf("%s: %w", message, err)
+const (
+	stackSize   = 10
+	skipNTraces = 4
+)
+
+func New(msg string) error {
+	err := errors.New(msg)
+	return Wrap(err, "")
 }
 
-func Wrapf(err error, format string, a ...any) error {
-	msg := fmt.Sprintf(format, a...)
-	return Wrap(err, msg)
+func Newf(format string, ctxValues ...interface{}) error {
+	msg := fmt.Sprintf(format, ctxValues...)
+	err := errors.New(msg)
+	return Wrap(err, "")
+}
+
+func Wrap(err error, context string) error {
+	stackTrace := extractStacktrace()
+	return &wrappedError{
+		err:        err,
+		context:    context,
+		stackTrace: stackTrace,
+	}
+}
+
+func Wrapf(err error, format string, ctxValues ...interface{}) error {
+	context := fmt.Sprintf(format, ctxValues...)
+	return Wrap(err, context)
+}
+
+type wrappedError struct {
+	err        error
+	context    string
+	stackTrace string
+}
+
+func (we *wrappedError) Error() string {
+	return fmt.Sprintf("%s: %v\n%s", we.context, we.err, we.stackTrace)
+}
+
+func (we *wrappedError) Unwrap() error {
+	return we.err
+}
+
+func extractStacktrace() string {
+	stack := make([]uintptr, stackSize)
+	length := runtime.Callers(skipNTraces, stack)
+	stack = stack[:length]
+	frames := runtime.CallersFrames(stack)
+
+	var builder strings.Builder
+	for frame, more := frames.Next(); more; frame, more = frames.Next() {
+		fmt.Fprintf(&builder, "\tat %s:%d: %s\n", frame.File, frame.Line, frame.Function)
+	}
+
+	return builder.String()
+}
+
+func Stacktrace(err error) string {
+	if wrappedErr, ok := err.(*wrappedError); ok {
+		return wrappedErr.Error()
+	}
+	return err.Error()
 }
