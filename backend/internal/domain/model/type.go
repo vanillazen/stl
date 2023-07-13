63e2463ad81f79dd9441f83cdecfc5362246b8f5
Adrian K <adrian.git@adrianpk.com>
2023-07-13 20:26:46 +0200

    Fixture now uses structs from model package

--- backend/internal/domain/model/type.go
+++ backend/internal/domain/model/type.go
@@ -1,32 +1,40 @@
 package model
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"strings"
 )
 
 type StringSlice []string
 
-func (s *StringSlice) Scan(value interface{}) error {
+func (s StringSlice) Scan(value interface{}) error {
 	switch v := value.(type) {
 	case []byte:
 		str := string(v)
 		if str == "" {
-			*s = []string{}
+			s = []string{}
 		} else {
-			*s = strings.Split(str, ",")
+			s = strings.Split(str, ",")
 		}
 	case string:
 		if v == "" {
-			*s = []string{}
+			s = []string{}
 		} else {
-			*s = strings.Split(v, ",")
+			s = strings.Split(v, ",")
 		}
 	case nil:
-		*s = []string{}
+		s = []string{}
 	default:
 		return fmt.Errorf("unsupported scan, storing driver.Value type %T into type *[]string", value)
 	}
 
 	return nil
+}
+
+func (s StringSlice) Value() (driver.Value, error) {
+	if len(s) == 0 {
+		return "", nil
+	}
+	return strings.Join(s, ","), nil
 }
