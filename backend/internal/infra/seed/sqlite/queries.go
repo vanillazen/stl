a37da19352779b9ca946f90af7021feeb8816c2d
Adrian K <adrian.git@adrianpk.com>
2023-07-14 12:07:02 +0200

    Naming updated

--- backend/internal/infra/seed/sqlite/queries.go
+++ backend/internal/infra/seed/sqlite/queries.go
@@ -1,22 +1,22 @@
 package sqlite
 
 const (
-	createSeederTable = `CREATE TABLE %s (
+	createSeedsTable = `CREATE TABLE %s (
     id UUID PRIMARY KEY,
     idx INTEGER,
     name VARCHAR(64),
     created_at TEXT
     );`
 
-	droopSeederTable = `DROP TABLE %s;`
+	dropSeedsTable = `DROP TABLE %s;`
 
-	selFromSeedsTable = `SELECT (COUNT(*) > 0) AS record_exists 
+	selectFromSeeds = `SELECT (COUNT(*) > 0) AS record_exists 
 		FROM %s 
 		WHERE idx = %d 
 		    AND name = '%s'`
 
-	insertSeederTable = `INSERT INTO %s (id, idx, name, created_at)
+	insertIntoSeeds = `INSERT INTO %s (id, idx, name, created_at)
 	VALUES (:id, :idx, :name, :created_at);`
 
-	deleteSeederTable = `DELETE FROM %s WHERE idx = %d AND name = '%s'`
+	deleteFromSeeds = `DELETE FROM %s WHERE idx = %d AND name = '%s'`
 )
